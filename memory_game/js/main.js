63713edfba54b29d281bb155025c4065f6aabbde
Chisomo Lungu <chisomo@gmail.com>
2017-04-26 14:16:26 +0100

    created game logic part 2

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,13 +1,28 @@
-//console.log("Up and running!"); 
 
-//creating the cards
+//creating the array of cards  
 
-var cardOne = "queen";
-var cardTwo = "queen"; 
-var cardThree = "king"; 
-var cardFour = "king";  
+var cards = ["queen", "queen", "king", "king"]; 
+var cardsInPlay = []; 
+var cardOne; 
+var cardTwo; 
 
-//checking the value of cards
+//using cardOne to hold the value of the first element in the cards array
+cardOne = cards[0]; 
+cardTwo = cards[2]; 
+//adding cardOne to the array via a push
+cardsInPlay.push(cardOne); 
+cardsInPlay.push(cardTwo); 
 
-console.log("You picked " + cardFour); 
-console.log("You flipped " + cardOne); 
+if (cardsInPlay.length === 2){
+	
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!"); 
+	} else {
+		alert("Sorry, try again"); 
+	}
+}
+else {
+	alert("Choose two cards");
+}
+console.log(cardTwo); 
+console.log(cardOne); 

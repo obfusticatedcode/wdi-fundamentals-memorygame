6d63a542ec0e32d015ec189a0ce7f02817abe0a8
Chisomo Lungu <chisomo@gmail.com>
2017-04-26 09:53:18 +0100

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,13 @@
+//console.log("Up and running!"); 
+
+//creating the cards
+
+var cardOne = "queen";
+var cardTwo = "queen"; 
+var cardThree = "king"; 
+var cardFour = "king";  
+
+//checking the value of cards
+
+console.log("You picked " + cardFour); 
+console.log("You flipped " + cardOne); 
